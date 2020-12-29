d743a826540588ac0a5b8be83b3bd0334c25e014
zhongweijie.x <zhongweijie.x@bytedance.com>
2020-12-29 15:45:06 +0800

    docs: 添加路由注解

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,10 @@ import { renderRoutes, RouteConfigComponentProps,  } from 'react-router-config';
 // 使用 Route 信息需要继承 RouteConfigComponentProps
 interface Props extends RouteConfigComponentProps {}
 
+/**
+ * 
+ * 嵌套路由需要在嵌套的组件内使用 renderRoutes 去渲染子路由
+ */
 function Home(props: Props) {
   const { route }  = props;
   return (
