a199f366bb21e2e178022315d01df492eb648871
zhongweijie.x <zhongweijie.x@bytedance.com>
2020-12-29 15:58:30 +0800

    feat: use styled-components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import { renderRoutes, RouteConfigComponentProps,  } from 'react-router-config';
+import React from "react";
+import { renderRoutes, RouteConfigComponentProps } from "react-router-config";
+import { Top } from "./style";
 
 // 使用 Route 信息需要继承 RouteConfigComponentProps
 interface Props extends RouteConfigComponentProps {}
 
 /**
- * 
+ *
  * 嵌套路由需要在嵌套的组件内使用 renderRoutes 去渲染子路由
  */
 function Home(props: Props) {
-  const { route }  = props;
+  const { route } = props;
   return (
     <>
-    <div>Home</div>
-    <div>
-      { renderRoutes(route?.routes)}
-    </div>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">WebApp</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      <div>{renderRoutes(route?.routes)}</div>
     </>
-  )
+  );
 }
 
 export default React.memo(Home);
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import globalStyle from '../../assets/style/global-style';
+
+export const Top = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 5px 10px;
+  background: ${globalStyle["theme-color"]};
+  & > span {
+    line-height: 40px;
+    color: #f1f1f1;
+    font-size: 20px;
+    &.iconfont {
+      font-size: 20px;
+    }
+  }
+`
