64d18e4f6d86acd588e09ebf6983aef45376694d
zhongweijie.x <zhongweijie.x@bytedance.com>
2020-12-29 20:02:45 +0800

    feat: 完成 Tab 部分样式

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { renderRoutes, RouteConfigComponentProps } from "react-router-config";
-import { Top } from "./style";
+import { NavLink } from "react-router-dom";
+import { Tab, TabItem, Top } from "./style";
 
 // 使用 Route 信息需要继承 RouteConfigComponentProps
 interface Props extends RouteConfigComponentProps {}
@@ -18,6 +19,24 @@ function Home(props: Props) {
         <span className="title">WebApp</span>
         <span className="iconfont search">&#xe62b;</span>
       </Top>
+
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected">
+          <TabItem>
+            <span>推荐</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/singers" activeClassName="selected">
+          <TabItem>
+            <span>歌手</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/rank" activeClassName="selected">
+          <TabItem>
+            <span>排行榜</span>
+          </TabItem>
+        </NavLink>
+      </Tab>
       <div>{renderRoutes(route?.routes)}</div>
     </>
   );
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -16,3 +16,42 @@ export const Top = styled.div`
     }
   }
 `
+
+export const Tab = styled.div`
+  height: 44px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  background: ${globalStyle["theme-color"]};
+  a {
+    flex: 1;
+    padding: 2px 0;
+    font-size: 14px;
+    color: #e4e4e4;
+    &.selected {
+      span {
+        padding: 3px 0;
+        font-weight: 700;
+        color: #f1f1f1;
+        position: relative;
+        &::after {
+          content: '';
+          height: 2px;
+          position: absolute;
+          left: 20%;
+          right: 20%;
+          bottom: -4px;
+          background: #fff;
+        }
+      }
+    }
+  }
+`
+
+export const TabItem = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
